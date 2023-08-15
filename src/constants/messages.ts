a5b56e9f15d2f0714fc2066e43ca140c62cf79e2
Salvador Bacci <salvador_bacci@hotmail.com>
2023-08-14 23:24:59 -0300

    📝 fixed welcome phrase

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,7 +1,7 @@
 import color from 'picocolors'
 
 export const introMessage = 
-`${color.cyan('Welcome to the Auto-linter CLI!')}
+`${color.cyan('Welcome to Auto-ESLint CLI!')}
                                                                           
    Let's setup your project's linter.
 `
